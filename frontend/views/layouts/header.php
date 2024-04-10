a332bca19560477d2787ba295e66e3dce80a8788
Huan <nguyenlienhuan1104@gmail.com>
2024-04-10 14:59:37 +0700

    push

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -15,7 +15,6 @@
 
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
             <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
-                <!-- <h1 class="fw-bold text-primary m-0">Mus<span class="text-secondary">cle</span>Food</h1> -->
                 <h1 class="fw-bold text-dark m-0" style="font-family: 'Arial', sans-serif;">Muscle<span class="text-secondary">Food</span></h1>
             </a>
             <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -50,4 +49,3 @@
             </div>
         </nav>
     </div>
-    <!-- Navbar End -->
